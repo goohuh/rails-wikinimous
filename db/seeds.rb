da1e47790406a99e75ab693410782d6f8dafaced
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-16 08:55:28 +0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  Article.create(title: Faker::TvShows::RuPaul.queen, content: Faker::Quote.matz)
+end
